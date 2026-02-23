af8a8b8ca138953ac097945e2fb0bf541909c578
m6rtini <148310527+m6rtini@users.noreply.github.com>
2026-02-23 12:30:27 +0200

    Add ApiResponse class for API response handling

---
+++ Helpers/ApiResponse.cs
@@ -0,0 +1,13 @@
+public class ApiResponse<T>
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public T Data { get; set; }
+
+    public ApiResponse(T data, string message = null)
+    {
+        Success = true;
+        Message = message;
+        Data = data;
+    }
+}
